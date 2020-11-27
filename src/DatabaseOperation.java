53488ada01f76eeb9ebc30246ee6129053368360
Muhammed Esad <esad.simitcioglu@ozu.edu.tr>
2020-11-27 20:14:46 +0300

    Database classes are created

---
+++ src/DatabaseOperation.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class DatabaseOperation {
+
+    public Connection con;
+    public Statement statement;
+    public PreparedStatement preparedStatement;
+
+    public DatabaseOperation(){
+
+    }
+
+
+
+
+}
