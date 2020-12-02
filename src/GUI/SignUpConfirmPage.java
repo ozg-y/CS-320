6a058d9cbc5b224eeeed996d34a17f9accb32d46
ozg-y <72527194+ozg-y@users.noreply.github.com>
2020-12-03 00:57:53 +0300

    Initial Commit

--- src/GUI/SignUpConfirmPage.java
+++ src/GUI/SignUpConfirmPage.java
@@ -1,19 +1,50 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.sql.SQLException;
+
+import Model.DatabaseOperation;
+import GUI.LoginPage;
+
+
 
 public class SignUpConfirmPage {
+    private DatabaseOperation op;
+    private JPanel panelC;
+    public JPanel getpanelC(){
+        return panelC;
+    }
     private JTextField textField1;
     private JButton button1;
+    private DatabaseOperation operation;
+    private JTextField email;
+    private JTextField code;
 
     public SignUpConfirmPage() {
+
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //todo-If code is correct go to LoginPage
                 //todo-If not stay
+                String mail = email.getText();
+                String text = code.getText();
+                int confirmation_code = Integer.parseInt(text);
+
+                try {
+                    if(confirmation_code == op.pull_student_confirmation_code(mail)){
+                        LoginPage login = new LoginPage();
+                        panelC.removeAll();
+                        panelC.add(login.getMainPanel());
+                    }
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+
             }
         });
     }
--- src/GUI/SignUpPage.java
+++ src/GUI/SignUpPage.java
@@ -3,6 +3,10 @@ package GUI;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.InputStream;
+
+import Model.Student;
+import Model.DatabaseOperation;
 
 public class SignUpPage {
     private JPanel panel1;
@@ -10,11 +14,36 @@ public class SignUpPage {
     private JPasswordField passwordField1;
     private JPasswordField passwordField2;
     private JButton signUpButton;
+    private DatabaseOperation operation;
+    private String studentName;
+    private String studentSurname;
+    private InputStream studentProfilePhoto;
+    private String studentEmail;
+    private String studentPassword;
+
 
     public SignUpPage() {
-        signUpButton.addActionListener(new ActionListener() {
+        signUpButton.addActionListener (new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String password1 = passwordField1.getText();
+                String password2 = passwordField2.getText();
+
+                if(password1.equals(password2)) {
+                    Student student = new Student(studentName,studentSurname,studentProfilePhoto,studentEmail,studentPassword);
+                      studentName = student.getStudentName();
+                      studentSurname = student.getStudentSurname();
+                      studentProfilePhoto = student.getStudentProfilePhoto();
+                      studentEmail = student.getStudentEmail();
+                      studentPassword = student.getStudentPassword();
+
+                    operation.push_student(studentName,studentSurname,studentProfilePhoto,studentEmail,studentPassword){
+                    //studentProfilePhoto string
+                        SignUpConfirmPage confirm = new SignUpConfirmPage();
+                        panel1.removeAll();
+                        panel1.add(confirm.getpanelC());
+                    }
+                }
 
             }
         });
