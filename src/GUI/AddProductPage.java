14e5526414340ab88b51583d7600fd65daed8315
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2020-12-04 15:40:24 +0300

    AddProductPage class designed.

--- src/GUI/AddProductPage.java
+++ src/GUI/AddProductPage.java
@@ -4,4 +4,8 @@ import javax.swing.*;
 
 public class AddProductPage {
     private JButton button1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextArea textArea1;
+    private JComboBox comboBox1;
 }
