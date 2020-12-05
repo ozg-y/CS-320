a52c3e3920e7b870ae80c4300e2f5447f536aaa4
aleynaolmezcan <aleyna.olmezcan@ozu.edu.tr>
2020-12-06 00:17:15 +0300

    Added ActionListener to ChangePasswordButton

--- src/GUI/ProfilePage.java
+++ src/GUI/ProfilePage.java
@@ -1,8 +1,25 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ProfilePage {
-    private JButton button1;
+    private JButton profilePhotoButton;
     private JButton changePasswordButton;
+
+    public ProfilePage() {
+        profilePhotoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        changePasswordButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
