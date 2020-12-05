437a9fabb6ace222134c0f0046fc1628658ce2ee
aleynaolmezcan <aleyna.olmezcan@ozu.edu.tr>
2020-12-06 00:16:49 +0300

    Designed ChangePasswordPage

---
+++ src/GUI/ChangePasswordPage.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChangePasswordPage {
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton changePasswordButton;
+
+    public ChangePasswordPage() {
+        changePasswordButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
