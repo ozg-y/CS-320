fd2624eb7ecdfeeecb030c2c32e72beaec4bc332
Muhammed Esad <esad.simitcioglu@ozu.edu.tr>
2020-12-09 01:01:50 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import GUI.HomePage;
+import Model.DatabaseOperation;
+
 import javax.swing.*;
 
 public class Main {
-    DatabaseOperation operation = new DatabaseOperation();
-    HomePage homeScreen = new HomePage(operation);
+
     public static void main(String[] args) {
+
+        DatabaseOperation operation = new DatabaseOperation();
+        HomePage homeScreen = new HomePage(operation);
 
     }
 }
