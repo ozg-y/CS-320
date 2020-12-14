40b98bfcffe86b9fb3548e734ceff2cca4d8f5ad
aleynaolmezcan <aleyna.olmezcan@ozu.edu.tr>
2020-12-14 19:57:06 +0300

    Added actionListener to the buttons

--- src/GUI/ProfilePage.java
+++ src/GUI/ProfilePage.java
@@ -1,8 +1,12 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import GUI.LPanel;
 public class ProfilePage {
     private JButton profilePhotoButton;
@@ -26,6 +30,25 @@ public class ProfilePage {
 
             }
         });
+
+        changePasswordButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                changePasswordButton.setBackground(Color.white);
+                changePasswordButton.setForeground(new Color(163,0,80));
+            }
+        });
+
+        changePasswordButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+                changePasswordButton.setBackground(new Color(163,0,80));
+                changePasswordButton.setForeground(Color.white);
+            }
+        });
+
 
     }
 
