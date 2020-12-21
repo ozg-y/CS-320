2dc01d3e7f84a9f75e4160969866fa5d0e80449f
burcula <burcu.arslan@ozu.edu.tr>
2020-12-22 00:58:57 +0300

    some changes

--- src/GUI/AddProductPage.java
+++ src/GUI/AddProductPage.java
@@ -38,6 +38,18 @@ public class AddProductPage {
         return addPanel;
     }
 
+    public static boolean isNumeric(String str) {
+        if (str == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
     public AddProductPage(DatabaseOperation operation, Student student,JFrame frame) {
 
 
@@ -48,22 +60,44 @@ public class AddProductPage {
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                productCategory = (String) comboBox1.getSelectedItem();
-                if (productCategory.equals("Book")) {
-                    operation.push_product(textField1.getText(), "book", Double.parseDouble(textField2.getText()),student.getStudentEmail(), textArea1.getText(),productPhotos);
-                } else if (productCategory.equals("Ticket")) {
 
-                    operation.push_product(textField1.getText(), "ticket", Double.parseDouble(textField2.getText()),student.getStudentEmail(), textArea1.getText(),productPhotos);
+                //if no photo is uploaded, user will not be able to add a new product
+                if(button1.isBorderPainted()){
+                    JOptionPane.showMessageDialog(null, "You must upload a photo to add a product.");
+                }
+                else if(textField1.getText().equals("")) { //if no title is written for the product, user will not be able to add a new product
+                    JOptionPane.showMessageDialog(null, "Title field cannot be blank.");
+                }
+                else if(textField2.getText().equals("")) {//if no price is written for the product, user will not be able to add a new product
+                    JOptionPane.showMessageDialog(null, "Price field cannot be blank.");
+                }
+                else if(isNumeric(textField2.getText())){ //the price is not entered as a numerical value
+                    JOptionPane.showMessageDialog(null, "Price must be a numerical value.");
+                }
+                else if(!comboBox1.isCursorSet()) {//if no label is chosen for the product, user will not be able to add a new product
+                    JOptionPane.showMessageDialog(null, "You must select a label.");
+                }
+                else if(textArea1.getText().equals("")) {//if no description is written for the product, user will not be able to add a new product
+                    JOptionPane.showMessageDialog(null, "You must provide a description for the product.");
+                }
+                else { // else push the product
 
-                } else if (productCategory.equals("Furniture")) {
+                    productCategory = (String) comboBox1.getSelectedItem();
+                    if (productCategory.equals("Book")) {
+                        operation.push_product(textField1.getText(), "book", Double.parseDouble(textField2.getText()), student.getStudentEmail(), textArea1.getText(), productPhotos);
+                    } else if (productCategory.equals("Ticket")) {
 
-                    operation.push_product(textField1.getText(), "furniture", Double.parseDouble(textField2.getText()),student.getStudentEmail(), textArea1.getText(),productPhotos);
+                        operation.push_product(textField1.getText(), "ticket", Double.parseDouble(textField2.getText()), student.getStudentEmail(), textArea1.getText(), productPhotos);
+
+                    } else if (productCategory.equals("Furniture")) {
+
+                        operation.push_product(textField1.getText(), "furniture", Double.parseDouble(textField2.getText()), student.getStudentEmail(), textArea1.getText(), productPhotos);
+
+                    }
+
+                    frame.getContentPane().removeAll();
 
                 }
-
-                frame.getContentPane().removeAll();
-
-
 
             }
         });
@@ -81,6 +115,7 @@ public class AddProductPage {
                 button1.setOpaque(true);
                 button1.setBorderPainted(false);
                 button1.setIcon(icon);
+
             }
         });
 
